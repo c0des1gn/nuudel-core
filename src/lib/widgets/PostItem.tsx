5b36a1817a337e175d10136b4d2ed13cd9a9e456
Damdinsuren Dorjsuren <damdinsuren@live.com>
2022-10-20 15:23:04 +0800

    post

--- src/lib/widgets/PostItem.tsx
+++ src/lib/widgets/PostItem.tsx
@@ -1,10 +1,10 @@
-import {IPostContent, ITagContent} from '../library/IBlog';
+import { IPostContent, ITagContent } from '../library/IBlog';
 import Date from './Date';
-import {Link, Text} from 'nuudel-core';
-import {titleShorter, decodeHTML} from 'nuudel-utils';
+import { Link, Text } from 'nuudel-core';
+import { titleShorter, decodeHTML } from 'nuudel-utils';
 import moment from 'moment';
 import styles from './styles.module.scss';
-import {getTextFromHtml} from '../common/helper';
+import { getTextFromHtml } from '../common/helper';
 import TagButton from './TagButton';
 
 interface Props {
@@ -12,17 +12,17 @@ interface Props {
   showDetail?: boolean;
 }
 
-export default function PostItem({post}: Props, props: Props) {
+export default function PostItem({ post }: Props, props: Props) {
   const tags: ITagContent[] = !post.tags
     ? []
-    : post.tags.map(tag => ({
+    : post.tags.map((tag) => ({
         name: tag,
         slug: tag,
       }));
 
   return (
     <div className="post-list-item">
-      <Link noLinkStyle href={'/posts/' + post.slug}>
+      <Link noLinkStyle href={'/post/' + post.slug}>
         <h2 className="post-list-title">{post.title}</h2>
       </Link>
       <p className="post-list-excerpt">
@@ -31,7 +31,7 @@ export default function PostItem({post}: Props, props: Props) {
         ) : !props.showDetail ? (
           titleShorter(getTextFromHtml(post.content), 512)
         ) : (
-          <div dangerouslySetInnerHTML={{__html: post.content}} />
+          <div dangerouslySetInnerHTML={{ __html: post.content }} />
         )}
       </p>
       <div className="post-list-metadata">
