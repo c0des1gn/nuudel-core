a6401feb6ef57a558c4567b048268762af47a4f0
Damdinsuren Dorjsuren <damdinsuren@live.com>
2023-06-08 17:42:14 +0800

    meta

--- src/lib/widgets/meta/OpenGraphMeta.tsx
+++ src/lib/widgets/meta/OpenGraphMeta.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { CONF } from 'nuudel-utils';
 import { IImage } from '../../../lib/common/Interfaces';
+import { isMobile } from 'react-device-detect';
 
 type Props = {
   url: string;
@@ -21,17 +22,14 @@ export default function OpenGraphMeta({
     height = undefined,
     uri: image = props.image,
   } = !props.image || typeof props.image === 'string' ? {} : props.image;
+  const link =
+    CONF.base_url +
+    (CONF.base_url?.endsWith('/') || url?.startsWith('/') ? '' : '/') +
+    url;
   return (
     <Head>
       <meta property="og:site_name" content={CONF.site_title} />
-      <meta
-        property="og:url"
-        content={
-          CONF.base_url +
-          (CONF.base_url?.endsWith('/') || url?.startsWith('/') ? '' : '/') +
-          url
-        }
-      />
+      <meta property="og:url" content={link} />
       <meta property="og:title" content={title || CONF.site_title} />
       <meta
         property="og:description"
@@ -39,6 +37,7 @@ export default function OpenGraphMeta({
       />
       <meta property="og:image" content={image || CONF.logo?.uri} />
       <meta property="og:type" content={type} />
+      {isMobile && !!link && <link rel="canonical" href={link} />}
       {!!width && <meta property="og:image:width" content={width + 'px'} />}
       {!!height && <meta property="og:image:height" content={height + 'px'} />}
     </Head>
