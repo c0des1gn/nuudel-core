c3d1b61d4a021c00f32e5466b9840c31c3ab35b0
Damdinsuren Dorjsuren <damdinsuren@live.com>
2023-08-03 19:19:51 +0800

    mobile

--- src/lib/components/Sortable/SortableItem.tsx
+++ src/lib/components/Sortable/SortableItem.tsx
@@ -1,10 +1,12 @@
-import React, {FC, createContext, useContext, useMemo} from 'react';
-import {useSortable, defaultAnimateLayoutChanges} from '@dnd-kit/sortable';
-import {CSS} from '@dnd-kit/utilities';
-import Item, {ItemProps} from './Item';
+import React, { FC, createContext, useContext, useMemo } from 'react';
+import { useSortable, defaultAnimateLayoutChanges } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import Item, { ItemProps } from './Item';
 import styles from './styles.module.scss';
-import type {CSSProperties} from 'react';
-import type {DraggableSyntheticListeners} from '@dnd-kit/core';
+import type { CSSProperties } from 'react';
+import type { DraggableSyntheticListeners } from '@dnd-kit/core';
+import { DeviceId } from '../../common/UI';
+import clsx from 'clsx';
 
 interface Context {
   attributes: Record<string, any>;
@@ -20,11 +22,11 @@ const SortableItemContext = createContext<Context>({
 });
 
 function animateLayoutChanges(args) {
-  const {isSorting, wasSorting} = args;
+  const { isSorting, wasSorting } = args;
   return isSorting || wasSorting ? defaultAnimateLayoutChanges(args) : true;
 }
 
-export const SortableItem: FC<ItemProps> = props => {
+export const SortableItem: FC<ItemProps> = (props) => {
   const {
     isDragging,
     attributes,
@@ -33,7 +35,7 @@ export const SortableItem: FC<ItemProps> = props => {
     transform,
     transition,
     setActivatorNodeRef,
-  } = useSortable({animateLayoutChanges, id: props.item?.uri || props.id});
+  } = useSortable({ animateLayoutChanges, id: props.item?.uri || props.id });
 
   const context = useMemo(
     () => ({
@@ -42,7 +44,7 @@ export const SortableItem: FC<ItemProps> = props => {
       ref: setActivatorNodeRef,
       onRemove: props.onRemove,
     }),
-    [attributes, listeners, setActivatorNodeRef],
+    [attributes, listeners, setActivatorNodeRef]
   );
 
   const style: CSSProperties = {
@@ -71,17 +73,18 @@ interface IRemoveProps {
 }
 
 export function RemoveItem<IRemoveProp>(prop: IRemoveProps) {
-  const {onRemove = undefined} = useContext(SortableItemContext);
+  const { onRemove = undefined } = useContext(SortableItemContext);
   return (
     <button
-      className={styles.removeItem}
+      className={clsx(styles.removeItem, DeviceId.isMobile && styles.onmobile)}
       type="button"
-      onClick={e => {
+      onClick={(e) => {
         if (onRemove) {
           onRemove!(prop.id);
         }
         //e.preventDefault();
-      }}>
+      }}
+    >
       <svg viewBox="0 0 22 22" width="8">
         <path d="M2.99998 -0.000206962C2.7441 -0.000206962 2.48794 0.0972617 2.29294 0.292762L0.292945 2.29276C-0.0980552 2.68376 -0.0980552 3.31682 0.292945 3.70682L7.58591 10.9998L0.292945 18.2928C-0.0980552 18.6838 -0.0980552 19.3168 0.292945 19.7068L2.29294 21.7068C2.68394 22.0978 3.31701 22.0978 3.70701 21.7068L11 14.4139L18.2929 21.7068C18.6829 22.0978 19.317 22.0978 19.707 21.7068L21.707 19.7068C22.098 19.3158 22.098 18.6828 21.707 18.2928L14.414 10.9998L21.707 3.70682C22.098 3.31682 22.098 2.68276 21.707 2.29276L19.707 0.292762C19.316 -0.0982383 18.6829 -0.0982383 18.2929 0.292762L11 7.58573L3.70701 0.292762C3.51151 0.0972617 3.25585 -0.000206962 2.99998 -0.000206962Z"></path>
       </svg>
@@ -90,7 +93,7 @@ export function RemoveItem<IRemoveProp>(prop: IRemoveProps) {
 }
 
 export function DragHandle() {
-  const {attributes, listeners, ref} = useContext(SortableItemContext);
+  const { attributes, listeners, ref } = useContext(SortableItemContext);
   return (
     <button
       className={styles.dragHandle}
@@ -98,7 +101,8 @@ export function DragHandle() {
       {...attributes}
       {...listeners}
       //onClick={e => { listeners?.onClick!(); e.preventDefault(); }}
-      ref={ref}>
+      ref={ref}
+    >
       <svg viewBox="0 0 20 20" width="12">
         <path d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"></path>
       </svg>
