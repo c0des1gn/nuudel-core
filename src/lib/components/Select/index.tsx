9d4252402fd2af70999ea4c1dc9af6b3c49dba71
Damdinsuren Dorjsuren <damdinsuren@live.com>
2024-03-19 08:46:22 +0800

    select

--- src/lib/components/Select/index.tsx
+++ src/lib/components/Select/index.tsx
@@ -6,6 +6,7 @@ type ISelectProps = SelectProps & {
   options: ISelectItem[];
   onChange?(e: any);
   children?: ReactNode;
+  variant?: 'standard' | 'outlined' | 'filled';
 };
 
 interface ISelectItem {
@@ -18,57 +19,71 @@ interface ISelectItem {
 export const Select: React.FC<ISelectProps> = React.forwardRef<
   HTMLSelectElement,
   ISelectProps
->(({ options, onChange, children, ...props }: ISelectProps, ref) => {
-  const handleChange = (event: SelectChangeEvent) => {
-    let value: any = event?.target?.value;
-    if (onChange) {
-      onChange(
-        !!value && value instanceof Array && value.length > 0 ? value[0] : value
-      );
-    }
-  };
-
-  const handleChangeMultiple = (event: SelectChangeEvent) => {
-    let value: any = event?.target?.value;
-    const { options = typeof value === 'string' ? value.split(',') : value } =
-      event.target as HTMLSelectElement;
-    const values: string[] = [];
-    for (let i = 0, l = options.length; i < l; i += 1) {
-      if (options[i].selected) {
-        values.push(options[i].value);
+>(
+  (
+    {
+      options,
+      onChange,
+      children,
+      variant = 'outlined',
+      ...props
+    }: ISelectProps,
+    ref
+  ) => {
+    const handleChange = (event: SelectChangeEvent) => {
+      let value: any = event?.target?.value;
+      if (onChange) {
+        onChange(
+          !!value && value instanceof Array && value.length > 0
+            ? value[0]
+            : value
+        );
       }
-    }
-    if (onChange) {
-      onChange(values);
-    }
-  };
+    };
 
-  /*
+    const handleChangeMultiple = (event: SelectChangeEvent) => {
+      let value: any = event?.target?.value;
+      const { options = typeof value === 'string' ? value.split(',') : value } =
+        event.target as HTMLSelectElement;
+      const values: string[] = [];
+      for (let i = 0, l = options.length; i < l; i += 1) {
+        if (options[i].selected) {
+          values.push(options[i].value);
+        }
+      }
+      if (onChange) {
+        onChange(values);
+      }
+    };
+
+    /*
   useEffect(() => {
     if (onChange) {
       onChange(props.value);
     }
   }, [props.value]); // */
 
-  return (
-    <BaseSelect
-      ref={ref}
-      {...props}
-      onChange={props.multiple ? handleChangeMultiple : handleChange}
-    >
-      {options.map((option: ISelectItem, i: number) => (
-        <MenuItem
-          key={i}
-          value={option?.value}
-          disabled={option.disabled === true}
-        >
-          {!!option?.icon && option.icon}
-          {option?.label}
-        </MenuItem>
-      ))}
-      {children}
-    </BaseSelect>
-  );
-});
+    return (
+      <BaseSelect
+        ref={ref}
+        {...props}
+        variant={variant}
+        onChange={props.multiple ? handleChangeMultiple : handleChange}
+      >
+        {options.map((option: ISelectItem, i: number) => (
+          <MenuItem
+            key={i}
+            value={option?.value}
+            disabled={option.disabled === true}
+          >
+            {!!option?.icon && option.icon}
+            {option?.label}
+          </MenuItem>
+        ))}
+        {children}
+      </BaseSelect>
+    );
+  }
+);
 
 export default Select;
